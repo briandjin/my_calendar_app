39b73a2eb3709ba599c5b447c3bef77f4545d105
Brian Jin <briandjin@gmail.com>
2018-02-23 16:03:23 -0600

    Create header and DatePicker component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import DatePicker from './components/DatePicker/DatePicker.js'
+import logo from './assets/RT-Header-Logo.jpg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state - {
+      date: "asdf",
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={logo} alt="logo" />
+          
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="main">
+        </div>
+        
       </div>
     );
   }
---
+++ src/components/DatePicker/DatePicker.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Calendar from 'react-calendar';
+import './DatePicker.css';
+
+class DatePicker extends Component {
+  render() {
+    return (
+      <Calendar />
+    );
+  }
+}
+
+export default DatePicker;
